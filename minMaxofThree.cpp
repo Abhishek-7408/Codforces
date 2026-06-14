1fcfed9e43345d9d43ad2161481362960ef83d29
Abhishek Kushwaha <abhishekkushwaha058@gmail.com>
2026-06-14 10:42:32 +0530

    final code added

---
+++ minMaxofThree.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    
+    cin >> a >> b >> c;
+     int mn =a;
+     int mx=a;
+     if(b<mn)
+     mn = b;
+     if(c<mn)
+     mn=c;
+     
+     if(b>mx)
+     mx = b;
+     if(c>mx)
+     mx = c;
+     
+     cout<< mn << " " << mx;
+     return 0;
+}
