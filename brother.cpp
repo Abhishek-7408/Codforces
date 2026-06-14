fee2e4069ab0450caf5aeca32c0c9c406ed25607
Abhishek Kushwaha <abhishekkushwaha058@gmail.com>
2026-06-14 10:49:04 +0530

    final code added

---
+++ brother.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace  std;
+int main(){
+    string a,b;
+    string c,d;
+    cin >> a >> b;
+    cin >> c >> d;
+    if (b==d)
+    cout<<"ARE Brothers";
+    else
+    cout<< "NOT";
+}
