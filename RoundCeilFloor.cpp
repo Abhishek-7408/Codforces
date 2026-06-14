3c4e418971f017fa125a7842b276ff77b8c7db5f
Abhishek Kushwaha <abhishekkushwaha058@gmail.com>
2026-06-14 10:18:46 +0530

    final code added

--- RoundCeilFloor.cpp
+++ RoundCeilFloor.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
-int main(){
-    int a,b;
-    cin >> a >> b;
-    cout << ""
+
+int main() {
+    long long A, B;
+    cin >> A >> B;
+
+    double x = (double)A / B;
+
+    cout << "floor " << A << " / " << B << " = " << (long long)floor(x) << endl;
+    cout << "ceil " << A << " / " << B << " = " << (long long)ceil(x) << endl;
+    cout << "round " << A << " / " << B << " = " << (long long)round(x) << endl;
+
+    return 0;
 }
