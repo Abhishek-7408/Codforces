ad96fd266b67626fee7d0e785921f6efca30ec45
Abhishek Kushwaha <abhishekkushwaha058@gmail.com>
2026-06-15 19:18:36 +0530

    added right code successfully

--- coOrdinates.cpp
+++ coOrdinates.cpp
@@ -7,6 +7,18 @@ int main(){
     cout<<"Origem";
     
     else if(x>0 && y>0)
+    cout<<"Q1";
+    else  if(x<0 && y>0)
+    cout<<"Q2";
+    else if(x>0 && y<0)
+    cout<<"Q4";
+    else if(x<0 && y<0)
+    cout<<"Q3";
+    else if(x==0)
+    cout<<"Eixo Y";
+    else
+    cout<<"Eixo X";
+
     
 
     return 0;
