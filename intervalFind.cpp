1cca4017b23f582656bcfa669af18be75692c04e
Abhishek Kushwaha <abhishekkushwaha058@gmail.com>
2026-06-16 22:44:03 +0530

    added right code successfully

--- intervalFind.cpp
+++ intervalFind.cpp
@@ -8,7 +8,7 @@ int main(){
     else if(x>25 && x<=50)
     cout<<"Interval (25,50]";
     else if(x>50 && x<=75)
-    cout<<"Interval (50,75)";
+    cout<<"Interval (50,75]";
     else if(x>75 && x<=100)
     cout<<"Interval (75,100]";
     else 
